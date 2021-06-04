d9034c9c8c19b40e77ce8099da0160aa27c7604b
vicmusa <59419072+vicmusa@users.noreply.github.com>
2021-06-04 12:10:17 -0400

    Update TesisFinal.ino

--- TesisFinal.ino
+++ TesisFinal.ino
@@ -7,7 +7,8 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heltec.h"
-#include "spo2_algorithm.h"
+#include "heartRate.h"
+MAX30105 particleSensor;
 
 /***** Definiciones *******/
 #define btn 12
@@ -28,6 +29,30 @@ WiFiManager wifiManager;
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
 
+/*********** VARIABLES MAX30102 *******/
+
+double avered = 0; double aveir = 0;
+double sumirrms = 0;
+double sumredrms = 0;
+int i = 0;
+int Num = 100;//calculate SpO2 by this sampling interval
+
+double ESpO2 = 95.0;//initial value of estimated SpO2
+double FSpO2 = 0.7; //filter factor for estimated SpO2
+double frate = 0.95; //low pass filter for IR/red LED value to eliminate AC component
+#define TIMETOBOOT 3000 // wait for this time(msec) to output SpO2
+#define SCALE 88.0 //adjust to display heart beat and SpO2 in the same scale
+#define SAMPLING 5 //if you want to see heart beat more precisely , set SAMPLING to 1
+#define FINGER_ON 30000 // if red signal is lower than this , it indicates your finger is not on the sensor
+#define MINIMUM_SPO2 80.0
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
+#define USEFIFO
 /******* METODOS *******/
 
 /*RECIBIR MENSAJE AWS */
@@ -121,7 +146,7 @@ void sendData()
    }
    publishMessage();
   }
-  delay(6000);
+ // delay(6000);
 }
 
 
@@ -162,7 +187,25 @@ void setupADC()
 /* INICIALIZANDO MAX30102 */
 void setupMAX30102()
 {
+  Wire1.begin(21, 22);
+while (!particleSensor.begin(Wire1, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30102 was not found. Please check wiring/power/solder jumper at MH-ET LIVE MAX30102 board. ");
+    //while (1);
+  }
 
+  //Setup to sense a nice looking saw tooth on the plotter
+  byte ledBrightness = 0x7F; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  //Options: 1 = IR only, 2 = Red + IR on MH-ET LIVE MAX30102 board
+  int sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  // Set up the wanted parameters
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  particleSensor.enableDIETEMPRDY();
 }
 /* INICIALIZANDO INTERRUPCIONES*/
 void setupISR()
@@ -183,6 +226,93 @@ void leerADC()
       promtemp=promtemp/100; // SE CALCULA EL PROMEDIO DE LA TEMP
      
 }
+void leerMax30102()
+{
+  Serial.println("estoy aqui");
+    uint32_t ir, red , green;
+  double fred, fir;
+  double SpO2 = 0; //raw SpO2 before low pass filtered
+
+#ifdef USEFIFO
+  particleSensor.check(); //Check the sensor, read up to 3 samples
+
+  while (particleSensor.available()) {//do we have new data
+#ifdef MAX30105
+   red = particleSensor.getFIFORed(); //Sparkfun's MAX30105
+    ir = particleSensor.getFIFOIR();  //Sparkfun's MAX30105
+#else
+    red = particleSensor.getFIFOIR(); //why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
+    ir = particleSensor.getFIFORed(); //why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
+#endif
+
+    
+    
+    i++;
+    fred = (double)red;
+    fir = (double)ir;
+    avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
+    aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
+    sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
+    sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
+    if ((i % SAMPLING) == 0) {//slow down graph plotting speed for arduino Serial plotter by thin out
+      if ( millis() > TIMETOBOOT) {
+        float ir_forGraph = (2.0 * fir - aveir) / aveir * SCALE;
+        float red_forGraph = (2.0 * fred - avered) / avered * SCALE;
+        //trancation for Serial plotter's autoscaling
+        if ( ir_forGraph > 100.0) ir_forGraph = 100.0;
+        if ( ir_forGraph < 80.0) ir_forGraph = 80.0;
+        if ( red_forGraph > 100.0 ) red_forGraph = 100.0;
+        if ( red_forGraph < 80.0 ) red_forGraph = 80.0;
+        //        Serial.print(red); Serial.print(","); Serial.print(ir);Serial.print(".");
+        if (ir < FINGER_ON) ESpO2 = MINIMUM_SPO2; //indicator for finger detached
+        float temperature = particleSensor.readTemperatureF();
+  
+        Serial.print(" Oxygen % = ");
+        Serial.println(ESpO2);
+      }
+    }
+    if ((i % Num) == 0) {
+      double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
+      // Serial.println(R);
+      SpO2 = -23.3 * (R - 0.4) + 100; //http://ww1.microchip.com/downloads/jp/AppNotes/00001525B_JP.pdf
+      ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2;//low pass filter
+      //  Serial.print(SpO2);Serial.print(",");Serial.println(ESpO2);
+      sumredrms = 0.0; sumirrms = 0.0; i = 0;
+      break;
+    }
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    //Serial.println(SpO2);
+  }
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
+  }
+   Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+#endif
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -209,5 +339,6 @@ void loop() {
  hr=random(80,100);
  spo2=random(95,100);
  promtemp=random(35,38);
+ leerMax30102();
  sendData();
 }
